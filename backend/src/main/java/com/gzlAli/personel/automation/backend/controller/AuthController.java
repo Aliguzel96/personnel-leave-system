1629b3a21fe8a3bb67160011df5e7cfd1f8903fb
aliguzel <aligzl1996@gmail.com>
2026-04-26 17:50:44 +0300

    api.js updated

--- backend/src/main/java/com/gzlAli/personel/automation/backend/controller/AuthController.java
+++ backend/src/main/java/com/gzlAli/personel/automation/backend/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.gzlAli.personel.automation.backend.dto.AuthResponse;
 import com.gzlAli.personel.automation.backend.dto.LoginRequest;
 import com.gzlAli.personel.automation.backend.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,16 @@ public class AuthController {
 
     private final JwtUtil jwtUtil;
 
+    @Value("${APP_USERNAME}")
+    private String appUsername;
+
+    @Value("${APP_PASSWORD}")
+    private String appPassword;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest req) {
 
-        if ("admin".equals(req.getUsername()) && "1234".equals(req.getPassword())) {
+        if (appUsername.equals(req.getUsername()) && appPassword.equals(req.getPassword())) {
             String token = jwtUtil.generate(req.getUsername());
             return ResponseEntity.ok(new AuthResponse(token));
         }
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "https://personnel-leave-system-z1w7.onrender.com/api"
 });
 
 //interceptor
